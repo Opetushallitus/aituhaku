6b6e29eed45df94ba7fa16bd3a048d310d03e427
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-02-26 11:49:41 +0200

    Korjattu skeema

--- src/clj/aituhaku/toimiala/skeema.clj
+++ src/clj/aituhaku/toimiala/skeema.clj
@@ -10,21 +10,19 @@
                (s/optional-key :koulutusala_nimi_sv) s/Str
                :tutkintotaso s/Str})
 
-(def Toimikunta {:tkunta s/Str
-                 :nimi_fi s/Str
-                 :nimi_sv s/Str
-                 :sahkoposti (s/maybe s/Str)
-                 :toimikausi_alku org.joda.time.LocalDate
-                 :toimikausi_loppu org.joda.time.LocalDate
-                 :kielisyys s/Str})
+(def ToimikuntaNimi {:nimi_fi s/Str
+                     :nimi_sv s/Str
+                     :tkunta s/Str})
+
+(def Toimikunta (merge ToimikuntaNimi
+                       {:sahkoposti (s/maybe s/Str)
+                        :toimikausi_alku org.joda.time.LocalDate
+                        :toimikausi_loppu org.joda.time.LocalDate
+                        :kielisyys s/Str}))
 
 (def Jarjestaja {:oppilaitoskoodi s/Str
                  :nimi s/Str})
 
-(def Toimikunta {:nimi_fi s/Str
-                 :nimi_sv s/Str
-                 :tkunta s/Str})
-
 (def TutkintoTiedot (merge Tutkinto
                            {:jarjestajat [Jarjestaja]
-                            :toimikunnat [Toimikunta]}))
+                            :toimikunnat [ToimikuntaNimi]}))
