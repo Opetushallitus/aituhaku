d7bea894564ce5d33bffdf1c591f1ea23d09694a
Jarno Kanto <Jarno.Kanto@solita.fi>
2014-06-23 11:02:10 +0300

    OPH-517: Loput ruotsinnokset. Poistettu tekstit_sv.sh jottei käännöksiä yliajeta.

--- dev-scripts/tekstit-sv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu
-
-REPO_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
-
-set -x
-
-cd $REPO_PATH/resources/i18n
-sed -E -e 's/(.+)/\1 (sv)/' tekstit.properties > tekstit_sv.properties
