9aa7bf1cd20e3522fe19fa4ef413b5ed860c9617
Antti Virtanen <antti.virtanen@iki.fi>
2014-09-29 11:20:42 +0300

    OPH-893 yksinkertainen suorituskyky testi peridot-kirjaston päällä. Tuki useille säikeille

--- test/clj/aituhaku/rest/performance_test.clj
+++ test/clj/aituhaku/rest/performance_test.clj
@@ -20,23 +20,52 @@
     (let [dev-asetukset (assoc haku-asetukset/oletusasetukset :development-mode true)
           asetukset (haku-asetukset/lue-asetukset dev-asetukset)]
       (alusta-korma! asetukset))))
- 
-(deftest ^:integraatio jarjestaja-tiedot-yksi-saie []  
+
+(defn with-korma [f]
+ (let [asetukset
+       (-> haku-asetukset/oletusasetukset
+         (assoc :development-mode true))
+       _ (alusta-korma! asetukset)]
+   (f)))
+
+(defn time-with-peridot-multi
+  "Provides simple performance test capabilities for Peridot. Runs random selection of URLs using specified amount of threads and reports results."
+  [url-seq thread-count urls-per-thread]
   (let [asetukset
         (-> haku-asetukset/oletusasetukset
           (assoc :development-mode true))
-        _ (alusta-korma! asetukset)
         crout (palvelin/app asetukset)
         _ (deliver haku-asetukset/asetukset asetukset)]
-
+    
     (let [peridot-session (peridot/session crout)
           start (java.lang.System/currentTimeMillis)]
-      (doseq [oppilaitos (take 100 (jarjestaja-arkisto/hae-oppilaitoskoodit))]
-          (let [response (peridot/request peridot-session (str "/api/jarjestaja/" (:oppilaitoskoodi oppilaitos)) :request-method :get)]
-            (is (= (:status (:response response)) 200))
-            ))
+      (let [urls-for-threads (for [_ (range thread-count)]
+                               (repeatedly urls-per-thread #(rand-nth url-seq)))]
+        (doall
+          (map deref
+            (doall
+              (for [thread-url-seq urls-for-threads]
+                (future
+                  (doseq [url url-seq]
+                    (let [response (peridot/request peridot-session url :request-method :get)]
+                      (is (= (:status (:response response)) 200))
+                      ))))))))
       (let [end (java.lang.System/currentTimeMillis)
             elapsed (- end start)
             avg (float (/ elapsed 100))]
         (log/info "average request time " avg " ms")
         ))))
+ 
+(defn oppilaitos->jarjestaja-url 
+  [oppilaitos]
+  (str "/api/jarjestaja/" (:oppilaitoskoodi oppilaitos)))
+
+(deftest ^:integraatio jarjestaja-tiedot-yksi-saie []
+  (with-korma
+    (let [urlit (map oppilaitos->jarjestaja-url (take 100 (jarjestaja-arkisto/hae-oppilaitoskoodit)))]
+      #(time-with-peridot-multi urlit 1 100))))
+
+(deftest ^:integraatio jarjestaja-tiedot-useita-saikeita []  
+  (with-korma
+    (let [urlit (map oppilaitos->jarjestaja-url (take 500 (jarjestaja-arkisto/hae-oppilaitoskoodit)))]
+      #(time-with-peridot-multi urlit 5 100))))
