cdd88431fbb021d2bf2130cdffd3d5750c039f3b
Heikki Hokkanen <heikki.hokkanen@solita.fi>
2014-10-08 12:57:42 +0300

    OPH-898: validointi koulutusaloille/opintoaloille.

--- src/clj/aituhaku/rest_api/opintoala.clj
+++ src/clj/aituhaku/rest_api/opintoala.clj
@@ -2,10 +2,11 @@
   (:require [compojure.core :as c]
             [schema.core :as schema]
             [aituhaku.rest-api.http-util :refer [json-response]]
-            [aituhaku.toimiala.skeema :refer [Opintoala]]
+            [aituhaku.toimiala.skeema :refer [Koulutusala]]
             [aituhaku.arkisto.opintoala :as arkisto]))
 
 (c/defroutes reitit
   (c/GET "/haku" [kieli]
     (json-response
-      (arkisto/hae kieli))))
+      (arkisto/hae kieli)
+      [Koulutusala])))
