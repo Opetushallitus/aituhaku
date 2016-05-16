5264fee448951bb3a1bacf30ead9281b245cca6d
Arttu Kaipiainen <arttu.kaipiainen@solita.fi>
2016-05-16 10:39:13 +0300

    Poistetaan clojure-utilsiin siirrettyä koodia

--- src/clj/aituhaku/rest_api/http_util.clj
+++ src/clj/aituhaku/rest_api/http_util.clj
@@ -5,21 +5,6 @@
     [aituhaku.asetukset :refer [asetukset]]
     [oph.common.util.http-util :refer [response-or-404]]))
 
-(defn json-response
-  ([data]
-   (if (nil? data)
-     {:status 404}
-     {:status 200
-      :body (cheshire/generate-string data)
-      :headers {"Content-Type" "application/json"
-                "Cache-control" (str "max-age=" (:response-cache-max-age @asetukset))}}))
-  ([data schema]
-   (json-response (s/validate (s/maybe schema) data))))
-
-(defn json-response-nocache
-  [data]
-  (assoc-in (json-response data) [:headers "Cache-control"] "max-age=0"))
-
 (defn response-nocache
   [data]
   (assoc-in (response-or-404 data) [:headers "Cache-control"] "max-age=0"))
